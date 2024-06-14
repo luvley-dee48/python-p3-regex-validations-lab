e5fb0a81db63e0f28428a80d4c50545ca2a31e6c
deborah <fabbydebby@gmail.com>
2024-06-14 16:26:35 +0300

    regex-validation

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,14 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
-name_regex = re.compile(name)
+name_pattern = r"^(?:[A-Z][a-z]*[-'\s]?)+$"
+name_regex = re.compile(name_pattern)
 
-phone_number = r""
-phone_regex = re.compile(phone_number)
 
-email_address = r""
-email_regex = re.compile(email_address)
+phone_pattern = r"^\+?\d{0,3}[-.\s]?(\(\d{3}\)|\d{3})[-.\s]?\d{3}[-.\s]?\d{4}$"
+phone_regex = re.compile(phone_pattern)
+
+
+email_pattern = r"^[a-zA-Z][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+email_regex = re.compile(email_pattern)
+
